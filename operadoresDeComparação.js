e27fcef3d9b4267eb225c424a81b33867caa629e
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-08 18:44:34 -0300

    Add files via upload

---
+++ operadoresDeComparação.js
@@ -0,0 +1,37 @@
+/*Criar duas variáveis para comparar se as duas são iguais
+  Criar duas variáveis para comparar se as duas são iguais e do mesmo tipo
+  Criar duas variáveis para comparar se as duas são diferentes
+  Criar duas variáveis para comparar se as duas são diferentes e também de outro tipo
+  Criar duas variáveis para comparar se a primeira variável é maior que a outra
+  Criar duas variáveis para comparar se a primeira variável é maior ou igual que a outra
+  Criar duas variáveis para comparar se a primeira variável é menor que a outra
+  Criar duas variáveis para comparar se a primeira variável é menor ou igual que a outra
+*/
+
+//Comparando se o valor é igual / Comparando se os valores são iguais e do mesmo tipo
+var a = 1;
+var b = 1;
+console.log(a == b);
+console.log(a === b);
+
+
+//Comparando se os valores são diferentes/ Comparando se os valores e tipos são diferentes
+var c = 2;
+var d = 2;
+console.log( c != d);
+console.log( c !== d);
+
+
+//Comparando se o valor é maior que o outro / Comparando se o valor é maior ou igual
+var e = 4;
+var f = 4;
+console.log( e > f);
+console.log( e >= f);
+
+//Comparando se o valor é menor / Comparando se o valor é menor ou igual
+var g = 6;
+var h = 6;
+console.log(g < h);
+console.log( g <= h);
+
+
